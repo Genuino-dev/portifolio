0193e8929d93460a3a7901df15605a087de5fc3c
Isabella Genuino <isabella.genuino@global.ntt>
2024-10-22 20:06:36 -0300

    fix(image): added image

--- src/components/AboutAccordion.tsx
+++ src/components/AboutAccordion.tsx
@@ -1,6 +1,13 @@
-import { Box, Grid, Stack, Typography, Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Stack,
+  Typography,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+} from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-
 const AboutAccordion = () => {
   return (
     <>
@@ -40,21 +47,19 @@ const AboutAccordion = () => {
               variant="h6"
               color="Primary"
             >
-              Graduanda em Análise e Desenvolvimento de Sistemas,
-              atualmente, sou desenvolvedora júnior, atuando como
-              desenvolvedora Frontend. Recebo positivos por minha
-              proatividade, desenvolvimento na entrega de projetos e pelo
-              apoio que ofereço à equipe e aos novos colaboradores. Tive
-              também a incrível oportunidade de passar sete meses na
-              Austrália, onde não só aperfeiçoei meu inglês, mas também
-              adquiri uma perspectiva global sobre tecnologias. Essa
-              experiência foi transformadora e me ajudou a crescer tanto
-              pessoal quanto profissionalmente. Sempre em busca de
-              aprendizado, tenho focado meus estudos em tecnologias de
-              Front-end e, nas horas vagas, gosto de explorar novas
-              tendências no mundo Frontend e participar de eventos. Estou
-              sempre aberta a debater e conversar sobre tecnologia e
-              oportunidades.
+              Graduanda em Análise e Desenvolvimento de Sistemas, atualmente,
+              sou desenvolvedora júnior, atuando como desenvolvedora Frontend.
+              Recebo positivos por minha proatividade, desenvolvimento na
+              entrega de projetos e pelo apoio que ofereço à equipe e aos novos
+              colaboradores. Tive também a incrível oportunidade de passar sete
+              meses na Austrália, onde não só aperfeiçoei meu inglês, mas também
+              adquiri uma perspectiva global sobre tecnologias. Essa experiência
+              foi transformadora e me ajudou a crescer tanto pessoal quanto
+              profissionalmente. Sempre em busca de aprendizado, tenho focado
+              meus estudos em tecnologias de Front-end e, nas horas vagas, gosto
+              de explorar novas tendências no mundo Frontend e participar de
+              eventos. Estou sempre aberta a debater e conversar sobre
+              tecnologia e oportunidades.
             </Typography>
           </Stack>
         </AccordionDetails>
@@ -80,48 +85,54 @@ const AboutAccordion = () => {
         <AccordionDetails>
           <Grid container justifyContent="center">
             <Stack direction="row" spacing={2}>
-              <Box
-                component="img"
+              <Box>
+                <img
                 src="src/assets/images/typescript.png"
                 alt="Typescript logo photo"
-                sx={{ width: 100, height: 100 }}
+                style={{ width: 100, height: 100 }}
               />
+              </Box>
               <Box
                 component="img"
-                src="/assets/images/images.png"
+                src="src/assets/images/images.png"
                 alt="React logo photo"
                 sx={{ width: 100, height: 100 }}
               />
-              <Box
-                component="img"
-                src="/assets/images/mongodb.png"
+              <Box>
+                <img
+                src="src/assets/images/mongodb.png"
                 alt="MongoDb logo photo"
-                sx={{ width: 100, height: 100 }}
+                style={{ width: 100, height: 100 }}
               />
-              <Box
-                component="img"
-                src="/assets/images/docker.jpg"
+              </Box>
+              <Box>
+                <img
+                src="src/assets/images/docker.jpg"
                 alt="Docker logo photo"
-                sx={{ width: 100, height: 100 }}
+                style={{ width: 100, height: 100 }}
               />
-              <Box
-                component="img"
-                src="/assets/images/azuredevops.png"
+              </Box>
+              <Box>
+                <img
+                src="src/assets/images/azuredevops.png"
                 alt="AzureDevops logo photo"
-                sx={{ width: 100, height: 100 }}
+                style={{ width: 100, height: 100 }}
               />
-              <Box
-                component="img"
-                src="/assets/images/github-6980894_1280.png"
+              </Box>
+              <Box>
+                <img
+                src="src/assets/images/github-6980894_1280.png"
                 alt="GitHub logo photo"
-                sx={{ width: 100, height: 100 }}
+                style={{ width: 100, height: 100 }}
               />
-              <Box
-                component="img"
-                src="/assets/images/nextjs.png"
-                alt="NextJs logo photo"
-                sx={{ width: 100, height: 100 }}
-              />
+              </Box>
+              <Box>
+                <img
+                  src="src/assets/images/nextjs.png"
+                  alt="NextJs logo photo"
+                  style={{ width: 100, height: 100 }}
+                />
+              </Box>
             </Stack>
           </Grid>
         </AccordionDetails>
