6e254b1008bf0f9ab8001e97ceb966113654e873
Isabella Genuino <isabella.genuino@global.ntt>
2025-03-10 16:48:03 -0300

    fix(hero): responsive settings into hero component

--- src/components/AboutAccordions/AboutSkills.tsx
+++ src/components/AboutAccordions/AboutSkills.tsx
@@ -1,64 +1,68 @@
-import { Accordion, AccordionDetails, AccordionSummary, Grid, Stack, Typography } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const AboutSkills = () => {
-    return (
-      <>
-        <Accordion
+  return (
+    <>
+      <Accordion
+        style={{
+          maxWidth: "90%",
+          margin: "0 auto",
+          fontSize: "20px",
+          backgroundColor: "transparent",
+        }}
+      >
+        <AccordionSummary
+          id="panel-header"
+          aria-controls="panel-content"
+          expandIcon={<ExpandMoreIcon />}
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <Grid container justifyContent="center">
+            <Typography variant="h6">Skills</Typography>
+          </Grid>
+        </AccordionSummary>
+        <AccordionDetails
           style={{
             maxWidth: "90%",
             margin: "0 auto",
-            fontSize: "20px",
+            fontSize: "18px",
             backgroundColor: "transparent",
           }}
         >
-          <AccordionSummary
-            id="panel-header"
-            aria-controls="panel-content"
-            expandIcon={<ExpandMoreIcon />}
-            style={{ display: "flex", justifyContent: "center" }}
-          >
-            <Grid container justifyContent="center">
-              <Typography variant="h6">Skills</Typography>
-            </Grid>
-          </AccordionSummary>
-          <AccordionDetails
-            style={{
-              maxWidth: "90%",
-              margin: "0 auto",
-              fontSize: "18px",
-              backgroundColor: "transparent",
-            }}
-          >
-            <Stack>
-              <Typography
-                style={{
-                  maxWidth: "100%",
-                  margin: "0 auto",
-                  fontSize: "14px",
-                }}
-                variant="h6"
-                color="primary"
-              >
-                Graduanda em Análise e Desenvolvimento de Sistemas, atualmente,
-                sou desenvolvedora júnior, atuando como desenvolvedora Frontend.
-                Recebo positivos por minha proatividade, desenvolvimento na
-                entrega de projetos e pelo apoio que ofereço à equipe e aos novos
-                colaboradores. Tive também a incrível oportunidade de passar sete
-                meses na Austrália, onde não só aperfeiçoei meu inglês, mas também
-                adquiri uma perspectiva global sobre tecnologias. Essa experiência
-                foi transformadora e me ajudou a crescer tanto pessoal quanto
-                profissionalmente. Sempre em busca de aprendizado, tenho focado
-                meus estudos em tecnologias de Front-end e, nas horas vagas, gosto
-                de explorar novas tendências no mundo Frontend e participar de
-                eventos. Estou sempre aberta a debater e conversar sobre
-                tecnologia e oportunidades.
-              </Typography>
-            </Stack>
-          </AccordionDetails>
-        </Accordion>
-      </>
-    );
-  };
-  
-  export default AboutSkills;
+          <Stack>
+            <Typography
+              style={{
+                maxWidth: "100%",
+                margin: "0 auto",
+                fontSize: "14px",
+              }}
+              variant="h6"
+              color="primary"
+            >
+              Sou uma desenvolvedora fullstack em constante evolução, focada em
+              criar soluções web robustas e eficientes. Atualmente, na NTT DATA,
+              trabalho com NestJS, Next.js e React.js, Node.js e Azure DevOps no
+              meu dia a dia. Acredito no aprendizado contínuo e, nas minhas
+              horas vagas, exploro novas tecnologias e ferramentas para expandir
+              meu conhecimento e me manter atualizada com as tendências do
+              mercado. Minha experiência em suporte técnico e um intercâmbio na
+              Austrália me proporcionaram habilidades valiosas em comunicação e
+              resolução de problemas. Busco desafios que me permitam aplicar
+              meus conhecimentos e contribuir para projetos inovadores.
+            </Typography>
+          </Stack>
+        </AccordionDetails>
+      </Accordion>
+    </>
+  );
+};
+
+export default AboutSkills;
--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -9,16 +9,16 @@ import { saveAs } from "file-saver";
 const Hero = () => {
   const StyledHero = styled("div")(({ theme }) => ({
     backgroundColor: theme.palette.primary.main,
-    height: "100vh",
+    minHeight: "100vh", // Alterado de height para minHeight para garantir que o componente ocupe pelo menos a altura da tela
     display: "flex",
     alignItems: "center",
     [theme.breakpoints.up("xs")]: {
-      // <= mobile
       paddingTop: "100px",
+      paddingBottom: "50px", // Adicionado paddingBottom para evitar que os botões fiquem muito próximos do componente About quando a tela é minimizada
     },
     [theme.breakpoints.up("md")]: {
-      // >=mobile
       paddingTop: "0",
+      paddingBottom: "0", // Adicionado paddingBottom para manter a consistência em telas maiores
     },
   }));
 
@@ -32,83 +32,69 @@ const Hero = () => {
     borderRadius: "50%",
     border: `1px solid ${theme.palette.primary.contrastText}`,
   }));
-  return (
-    <>
-      <StyledHero>
-        <Container maxWidth="lg">
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={5}>
-              <Box position="relative">
-                <Box position="absolute" width={"100%"} top={-100} right={0}>
-                  <AnimatedBackground />
-                </Box>
-                <Box position="relative" textAlign="center">
-                  <StyledImg src={avatar} />
-                </Box>
-              </Box>
-            </Grid>
-            <Grid item xs={12} md={7} marginTop="50px">
-              <Typography
-                color="primary.contrastText"
-                variant="h2"
-                textAlign="center"
-                pb={2}
-                marginTop="50px"
-              >
-                Isabella Genuino
-              </Typography>
-              <Typography
-                color="primary.contrastText"
-                variant="h4"
-                textAlign="center"
-              >
-                I'm Software Developer
-              </Typography>
-              <Grid
-                container
-                display="flex"
-                justifyContent="center"
-                spacing={3}
-                pt={3}
 
-              >
-                <Grid
-                  item
-                  xs={12}
-                  md={4}
-                  display="flex"
-                  justifyContent="center"
+  return (
+    <StyledHero>
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={5}>
+            <Box position="relative">
+              <Box position="absolute" width={"100%"} top={-100} right={0}>
+                <AnimatedBackground />
+              </Box>
+              <Box position="relative" textAlign="center">
+                <StyledImg src={avatar} />
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={7} marginTop="50px">
+            <Typography
+              color="primary.contrastText"
+              variant="h2"
+              textAlign="center"
+              pb={2}
+              marginTop="50px"
+            >
+              Isabella Genuino
+            </Typography>
+            <Typography
+              color="primary.contrastText"
+              variant="h4"
+              textAlign="center"
+            >
+              I'm Software Developer
+            </Typography>
+            <Grid
+              container
+              display="flex"
+              justifyContent="center"
+              spacing={3}
+              pt={3}
+            >
+              <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                <StyledButton onClick={downloadCurriculum}>
+                  <FileDownloadIcon fontSize="medium" />
+                  <Typography> Download CV</Typography>
+                </StyledButton>
+              </Grid>
+              <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                <a
+                  href="https://www.linkedin.com/in/isabellagenuino/"
+                  target="_blank" // Corrigido de target="_blanck" para target="_blank"
+                  rel="noopener noreferrer" // Corrigido de rel="nooperner noreferrer" para rel="noopener noreferrer"
+                  style={{ textDecoration: "none" }}
                 >
-                  <StyledButton onClick={downloadCurriculum}>
-                    <FileDownloadIcon  fontSize="medium"/>
-                    <Typography> Download CV</Typography>
+                  <StyledButton>
+                    <ContactMailIcon fontSize="medium" />
+                    <Typography>Contact me</Typography>
                   </StyledButton>
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  md={4}
-                  display="flex"
-                  justifyContent="center"
-                >
-                  <a
-                    href="https://www.linkedin.com/in/isabellagenuino/"
-                    target="_blanck"
-                    rel="nooperner noreferrer"
-                    style={{ textDecoration: "none" }}
-                  >
-                    <StyledButton>
-                      <ContactMailIcon fontSize="medium" />
-                      <Typography>Contact me</Typography>
-                    </StyledButton>
-                  </a>
-                </Grid>
+                </a>
               </Grid>
             </Grid>
           </Grid>
-        </Container>
-      </StyledHero>
-    </>
+        </Grid>
+      </Container>
+    </StyledHero>
   );
 };
 
